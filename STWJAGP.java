f1937b070365a06d735834698f07cc902a596335
Sora0920 <sora0920@users.noreply.github.com>
2017-07-21 11:54:43 +0900

    文字列反転を実装

--- STWJAGP.java
+++ STWJAGP.java
@@ -3,6 +3,7 @@ import javax.print.DocFlavor.STRING;
 import javax.swing.*;
 import java.util.regex.*;
 import java.util.Random;
+
 // 名前の由来: Something that was just a greeting program
 // 改造しすぎて趣旨が変わってきたので名前を変えたけど実質hello100.javaです
 class STWJAGP {
@@ -18,6 +19,7 @@ class STWJAGP {
         Pattern pEcho = Pattern.compile("^echo |^echo$");
         Pattern pName = Pattern.compile("^name |^name$");
         Pattern pRand = Pattern.compile("^rand [0-9]{0,9}$|^rand$");
+        Pattern pReve = Pattern.compile("^reve |^reve$");
         Random Rnd = new Random();
         System.out.println("説明: HELPと入力");
         try {
@@ -35,6 +37,7 @@ class STWJAGP {
                 Matcher mEcho = pEcho.matcher(str_cat);
                 Matcher mName = pName.matcher(str_cat);
                 Matcher mRand = pRand.matcher(str_cat);
+                Matcher mReve = pReve.matcher(str_cat);
                 if (str_cat.equals("exit")) {
                     System.out.println("\nGoodBye");
                     System.exit(0);
@@ -42,7 +45,18 @@ class STWJAGP {
                 }
                 if (str_cat.equals("help")) {
                     System.out.println(
-                            "\n--HELP--\n100回挨拶するかExitと打つまで終了できません\nExit: 終了\necho: 名前の通りエコー命令です\n  echo [テキスト]と打つとテキストの部分が帰ってきます\nName: Helloって言ってくるやつに名前をつけられます\n  name [名前]でnameの部分が名前として設定されます\n  nameのみ打つと名前機能が解除されます\nRand: 乱数です。以上\n  Rand [数値]で0から数値の値までの乱数を生成します\n  Rand空打ちで0から2147483647までの乱数を生成します\nCheat: チートできます\n  Cheatの命令: \n     kill 全体の人数を指定された人数消せる\n     fcon 得点を追加できる\n  Cheatの書式: Cheat [-99~999](三桁で入力してください) [命令]\n※入力は大文字小文字関係なく認識されます。\n--HELP--\n");
+                            "\n--HELP--\n100回挨拶するかExitと打つまで終了できません\nExit: 終了\necho: 名前の通りエコー命令です\n  echo [テキスト]と打つとテキストの部分が帰ってきます\nName: Helloって言ってくるやつに名前をつけられます\n  name [名前]でnameの部分が名前として設定されます\n  nameのみ打つと名前機能が解除されます\nRand: 乱数です。以上\n  Rand [数値]で0から数値の値までの乱数を生成します\n  Rand空打ちで0から2147483647までの乱数を生成します\nReve: 文字列反転です\n  Reve [文字列]で文字列の部分を反転させます\nCheat: チートできます\n  Cheatの命令: \n     kill 全体の人数を指定された人数消せる\n     fcon 得点を追加できる\n  Cheatの書式: Cheat [-99~999](三桁で入力してください) [命令]\n※入力は大文字小文字関係なく認識されます。\n--HELP--\n");
+                    continue;
+                }
+                if (mReve.find()){
+                    if(str_cat.length() > 5){
+                        String str_reve_cat = str.substring(5, str.length());
+                        StringBuffer str_reve = new StringBuffer(str_reve_cat);
+                        String rev = str_reve.reverse().toString();
+                        System.out.println("\n--Reverse--\n" + rev + "\n--Reverse--\n");
+                    }else{
+                        System.out.println("\n--Reverse--\nError: 内容がありません\n--Reverse--\n");
+                    }
                     continue;
                 }
                 if (mRand.find()){
